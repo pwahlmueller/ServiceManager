7a7f2984971bb4a1aa9d09bdb54255f1588b626b
pwahlmueller <patrick@wahlmueller.eu>
2020-09-22 12:09:27 +0200

    Start SR

---
+++ StartSR.ps1
@@ -0,0 +1,19 @@
+﻿#
+# Sometimes ServiceRequests get stuck and do not start
+# This starts the sr
+#
+# Change the status of a manual activity to active 
+
+Import-Module SMLets -Force
+
+$MANR = 'MA716479'
+$SRNR = 'MA0000'
+
+$smdefaultcomputer = 'scsm'
+
+$MAClass = Get-SCSMClass -Name System.WorkItem.Activity.ManualActivity$
+$SRClass = Get-SCSMClass -Name System.WorkItem.servicerequest$
+$MAObject = Get-SCSMObject -Class $MAClass -filter "ID -eq $MANR"
+Set-SCSMObject -SMObject $MAObject -Property Status -Value Active
+$sr = Get-SCSMObject -Class $SRAClass -filter "ID -eq $SRNR"
+Set-SCSMObject -SMObject $SR  -Property Status -Value completed
